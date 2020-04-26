306dfa3b3b31235e7d170b2711b484baa0cc8a7b
Hani Majebry <hani.majebry@gmail.com>
2020-04-26 19:26:05 +0200

    Setup VueRouter, Vuex, and VueBootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router.js'
+import store from './store.js'
+import { BootstrapVue } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container>
+    <b-card title="Home" class="mt-5">
+      <b-card-text>This is the home page!</b-card-text>
+    </b-card>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
